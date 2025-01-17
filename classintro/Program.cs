d1c19584f067fa7f15e399dacc6b5f64885315ad
Mustafa ARSLAN <mustafacapik077@gmail.com>
2025-01-17 23:03:32 +0300

    dogru izlenme oranları

--- classintro/Program.cs
+++ classintro/Program.cs
@@ -10,21 +10,20 @@
         kurs1.kursunEgitmeni = "engin demiroğ";
         kurs1.izlenmeOrani = 68;
 
+        Kurs kurs4 = new Kurs();
+        kurs4.kursAdi = "HERBOKOLOG";
+        kurs4.kursunEgitmeni = "MUSTAFA ARSLAN ";
+        kurs4.izlenmeOrani = 500;
+
         Kurs kurs2 = new Kurs();
         kurs2.kursAdi = "Java";
         kurs2.kursunEgitmeni = "celil unver";
-        kurs2.izlenmeOrani = 64;
-
+        kurs2.izlenmeOrani = kurs4.izlenmeOrani + 5;
 
         Kurs kurs3 = new Kurs();
         kurs3.kursAdi = "phyton";
         kurs3.kursunEgitmeni = "halil unver ";
         kurs3.izlenmeOrani = 200;
-
-        Kurs kurs4 = new Kurs();
-        kurs4.kursAdi = "HERBOKOLOG";
-        kurs4.kursunEgitmeni = "MUSTAFA ARSLAN ";
-        kurs4.izlenmeOrani = 500;
 
 
         Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3,kurs4 };
